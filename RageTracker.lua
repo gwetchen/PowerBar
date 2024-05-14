310931bb011a7a5d5c0074f27f205ba9fa39701f
gretchen <gretchenn@gmail.com>
2024-05-14 13:41:10 +0200

    really break rage

--- RageTracker.lua
+++ RageTracker.lua
@@ -10,10 +10,7 @@ RageTracker_OnEvent = function(frame, event, arg1)
   frame:EnableMouse(unlocked)
 end
 RageTrackerFill_OnEvent = function(frame, event, arg1)
-  if event == "UNIT_RAGE" and UnitIsUnit(arg1,"player") then
-    frame:SetWidth(UnitMana("player")*(RageTrackerFrame:GetWidth())/100+1)
-    RageTrackerFrame_TextFrame_Text:SetText("Rage: "..UnitMana("player"))
-  elseif event == "UNIT_ENERGY" and UnitIsUnit(arg1,"player") then
+  if event == "UNIT_ENERGY" and UnitIsUnit(arg1,"player") then
     frame:SetWidth(UnitMana("player")*(RageTrackerFrame:GetWidth())/100+1)
     RageTrackerFrame_TextFrame_Text:SetText("Energy: "..UnitMana("player"))
   end
