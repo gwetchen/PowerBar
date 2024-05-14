af045aeed746f412a546b4ce26659bb09d1ec9ba
gretchen <gretchenn@gmail.com>
2024-05-14 13:38:34 +0200

    yep

--- RageTracker.lua
+++ RageTracker.lua
@@ -13,7 +13,10 @@ RageTrackerFill_OnEvent = function(frame, event, arg1)
   if event == "UNIT_RAGE" and UnitIsUnit(arg1,"player") then
     frame:SetWidth(UnitMana("player")*(RageTrackerFrame:GetWidth())/100+1)
     RageTrackerFrame_TextFrame_Text:SetText("Rage: "..UnitMana("player"))
-  end 
+  elseif event == "UNIT_ENERGY" and UnitIsUnit(arg1,"player") then
+    frame:SetWidth(UnitMana("player")*(RageTrackerFrame:GetWidth())/100+1)
+    RageTrackerFrame_TextFrame_Text:SetText("Energy: "..UnitMana("player"))
+  end
 end
 SlashCmdList["RAGETRACKER_COMMAND"] = function(Flag)
 	local flag = string.lower(Flag)
